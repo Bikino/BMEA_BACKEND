1bd13e0efede0918a1ee0e777ac5ea5e955d55bc
bikino <bikilde@gmail.com>
2025-07-26 00:28:46 -0400

    Initial commit

---
+++ bmea_backend/src/main/java/com/bkn/bmea_backend/BmeaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bkn.bmea_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmeaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmeaBackendApplication.class, args);
+	}
+
+}
---
+++ bmea_backend/src/test/java/com/bkn/bmea_backend/BmeaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bkn.bmea_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmeaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
